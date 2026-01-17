2ad8ff2056fbb6308ac1b61eb69291afd118e2bc
david gallegos <davidgallegos@davids-MacBook-Air.local>
2026-01-16 21:09:05 -0600

    zoneless

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
-import { provideExperimentalZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { bootstrapApplication } from "@angular/platform-browser";
+import { provideHttpClient } from "@angular/common/http";
+import { provideRouter } from "@angular/router";
 
-import { AppComponent } from './app/app-shell/app.component';
-import { appRoutes } from './app/app.routes';
+import { AppComponent } from "./app/app-shell/app.component";
+import { appRoutes } from "./app/app.routes";
+import { provideZonelessChangeDetection } from "@angular/core";
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
     provideRouter(appRoutes),
-    provideExperimentalZonelessChangeDetection(),
+    provideZonelessChangeDetection(),
   ],
 }).catch((error) => console.error(error));
